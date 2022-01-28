806eccd48a7e89db628fa1808d8881393da4cf5a
Victor Bezerra <victorbzrra@alu.ufc.br>
2022-01-27 21:34:59 -0300

    Component: Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,8 @@ export default class MyDocument extends Document{
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;1,400&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
+        
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
